86e07517c311e95daa6eea8fe4f391f4c0e488b7
aniskhan <anisa@khandkar.net>
2018-02-03 18:29:30 -0500

    working to spec

--- HW1/2/clean/Main.java
+++ HW1/2/clean/Main.java
@@ -13,24 +13,37 @@ import java.lang.Math; // do I need this if I explicitly call Math?
 public class Main {
 
 	public static void main(String[] args) {
-	//BufferedReader/InputStreamReader/IO Exception portions adapted from:
+		//BufferedReader/InputStreamReader/IO Exception portions adapted from:
     	//https://stackoverflow.com/questions/4644415/java-how-to-get-input-from-system-console
 
 	   BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	   while (true) { // TODO: exit on EOF
-		Double circleRadius = null;
-		Double circleArea = null;
-		System.out.print("Enter a circle radius (numeric value only): ");
-		try {
-			circleRadius = Double.parseDouble(reader.readLine());
-		} catch (NumberFormatException | IOException ex) {
-			System.err.println("Hey! Numbers only... \n"); // System.err makes it print in red :)
-			continue;
-		}
-		circleArea = Math.PI * (Math.pow(circleRadius, 2)); // https://www.tutorialspoint.com/java/lang/math_pow.htm
-    	    	System.out.println("Your radius: " + circleRadius);
-    	    	System.out.println("... means your circle has an area of: " + circleArea + "\n");
+	   while (true) {
+			Double circleRadius = null;
+			Double circleArea = null;
+			System.out.print("Enter a circle radius (numeric value only): ");
+			try {
+				String input = reader.readLine();
+				if (input !=null) { // no EOF provided
+					try {
+						circleRadius = Double.parseDouble(input);
+						
+					} catch (NumberFormatException ex) {
+						System.err.println("Hey! Numbers only... \n"); // System.err makes it print in red :)
+						continue;
+					}
+					circleArea = Math.PI * (Math.pow(circleRadius, 2)); // https://www.tutorialspoint.com/java/lang/math_pow.htm
+					System.out.println("Your radius: " + circleRadius);
+					System.out.println("... means your circle has an area of: " + circleArea + "\n");
+				} else {
+					System.out.print("Goodbye!");
+					System.exit(0);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		   
 	   }
 
 	}
 }
+
--- HW1/2/src/calcCircleArea/Main.java
+++ HW1/2/src/calcCircleArea/Main.java
@@ -9,30 +9,40 @@ package calcCircleArea;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.Math; // do I need this if I explicitly call Math?
+import java.lang.Math;
 
 public class Main {
 
 	public static void main(String[] args) {
 		//BufferedReader/InputStreamReader/IO Exception portions adapted from:
     	//https://stackoverflow.com/questions/4644415/java-how-to-get-input-from-system-console
-    	
+
 	   BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	   start://  
-	   while (true) {
+	   while (true) { // TODO: exit on EOF
+			Double circleRadius = null;
+			Double circleArea = null;
+			System.out.print("Enter a circle radius (numeric value only): ");
+			try {
+				String input = reader.readLine();
+				if (input !=null) { // no EOF provided
+					try {
+						circleRadius = Double.parseDouble(input);
+						
+					} catch (NumberFormatException ex) {
+						System.err.println("Hey! Numbers only... \n"); // System.err makes it print in red :)
+						continue;
+					}
+					circleArea = Math.PI * (Math.pow(circleRadius, 2)); // https://www.tutorialspoint.com/java/lang/math_pow.htm
+					System.out.println("Your radius: " + circleRadius);
+					System.out.println("... means your circle has an area of: " + circleArea + "\n");
+				} else {
+					System.out.print("Goodbye!");
+					System.exit(0);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		   
-		   Double circleRadius = null;
-		   Double circleArea = null;
-		   System.out.print("Enter a circle radius (numeric value only): ");
-		try {
-			circleRadius = Double.parseDouble(reader.readLine());
-		} catch (NumberFormatException | IOException ex) {
-			System.err.println("Hey! Numbers only... \n"); // System.err makes it print in red :)
-			continue start;	
-		}
-			circleArea = Math.PI * (Math.pow(circleRadius, 2)); // https://www.tutorialspoint.com/java/lang/math_pow.htm
-    	    System.out.println("Your radius: " + circleRadius);
-    	    System.out.println("... means your circle has an area of: " + circleArea + "\n");
 	   }
 
 	}
