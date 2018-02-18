b0b6fe01527b66a381d0ec97c3dba11942883c6e
aniskhan <anisa@khandkar.net>
2018-02-18 10:47:48 -0500

    refactor
    making this much simpler

--- HW2/1/src/Main.java
+++ HW2/1/src/Main.java
@@ -1,6 +1,6 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 
 /*Question 1:
@@ -16,86 +16,54 @@ program shows:
   
 
 /*My Approach:
- For this to work efficiently, the same function must be performed on each row.
- To better visualize what's happening here, it best to treat this as a square
- matrix, while selectively assigning and/or printing the numbers that form the pyramid.
  
- So the above example should be conceptualized as:
--5 -4 -3 -2 -3 -4 -5 
--4 -3 -2 -1 -2 -3 -4 
--3 -2 -1  0 -1 -2 -3 
+ The above example should be conceptualized as:
+
 -2 -1  0  1  0 -1 -2 
 -1  0  1  2  1  0 -1 
  0  1  2  3  2  1  0 
  1  2  3  4  3  2  1 
  
  From here it's simple to see that in order to hold the pyramid, you need a matrix with n rows,
- where n = height + 2(height-1) AND that each row has a seed (starting point) relative to x / y.
- The values count up from that seed to the center of the pyramid, then decrease.
+ where n = height + (height-1) AND that each row has a seed (starting point) relative to x / y.
+ The values count up from that seed to the center of the pyramid, then decreases. 
  
- To display the pyramid, you can simply assign the value to matrix or (print to console) if it is greater than 0.
+ To display the pyramid, you can simply print to console if the value is greater than 0.
  * 
  * */
 
 class Main {
-	static int pyramidHeight = 0;
-	static int rowCount;
-	static int colCount;
-	static int [][] matrix;
-	static int centerColIndex;
 	public static void main(String[] args) {	
-		while(true) {
+		Scanner input = new Scanner(System.in);
 			System.out.println("How tall would you like your pyramid to be?");
-			if (parseStoreInput()) {
-				matrix = new int[rowCount][colCount];
-				populatePyramid();
-				printMatrix();
-			} else {
+			try{
+				int pyramidHeight = input.nextInt();
+				int rCount = pyramidHeight;
+				int kCount = pyramidHeight + (pyramidHeight- 1); // how many columns needed
+				int centerkIndex = pyramidHeight;
+				
+				for (int r = 1; r <= rCount; r++) {
+					Integer seed = (-rCount+1+r); // this is the number each row should begin at in terms of x
+					System.out.println();
+					for (int k = 1; k <= kCount; k++){
+						if(seed > 0) { // only print if value is a part of printable pyramid
+							 System.out.print(seed.toString());				 
+						 } else {
+							 System.out.print(" ");	
+						 }
+						 //increment based on which side of pyramid we are on
+						 if(k < centerkIndex) { // intentionally do not want to increment at centerColIndex
+							 seed++;
+						 } else {
+							 seed--;					  
+						 }
+					}
+				}
+			}
+			catch (InputMismatchException | NumberFormatException e){
 				System.err.println("Unable to parse input. "
 						+ "Enter a positive whole number.");
-				continue;
 			}
-		}
-	}
-	public static boolean parseStoreInput() {
-		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			Integer input = Integer.parseUnsignedInt(reader.readLine());
-			pyramidHeight = input; // pyramid height
-			rowCount = pyramidHeight + (pyramidHeight- 1); // how many rows needed in each array?
-			colCount = rowCount;
-			centerColIndex = pyramidHeight- 1; //adjusted for 0-indexed array
-		} catch (IOException | NumberFormatException e){
-			return false;
-		}
-		return true;
-	}
-	public static void populatePyramid() {
-		for (int x = 0;x < matrix.length; x++) {
-			int seed = (-rowCount+2+x); // this is the number each row should begin at in terms of x
-			 for (int y = 0; y < matrix.length; y++) {
-				 if(seed > 0) { // only allocate if number will print
-					 matrix[x][y] = seed;						 
-				 }
-				 //increment based on which side of pyramid we are on
-				 if(y < centerColIndex) { // intentionally do not want to increment at centerColIndex
-					 seed++;
-				 }else {
-					 seed--;					  
-				 }
-			 }			
-		}
-	}
-	public static void printMatrix() {
-		for (int x = 0; x < matrix.length; x++) {
-		    for (int y = 0; y < matrix.length; y++) {
-		    	if(matrix[x][y] != 0) {
-		    		System.out.print(matrix[x][y] + " "); //TODO formatting with .printf
-		    	} else {
-		    		System.out.print(" " + " ");
-		    	}
-		    }
-		    System.out.println();
-		}
+			input.close();
 	}
 }
---
+++ HW2/2/src/Main.java
@@ -0,0 +1,32 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*Question 2: 
+Write a program that displays a calendar on the screen. It asks the user 
+the year, what day the first of January fell on, then displays every month 
+from January to December. Make sure to get the leap years and the number 
+of days in each month right. Your months should resemble the following 
+format:
+
+   February 2015      
+Su Mo Tu We Th Fr Sa  
+ 1  2  3  4  5  6  7  
+ 8  9 10 11 12 13 14  
+15 16 17 18 19 20 21  
+22 23 24 25 26 27 28 */
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		while (true) {
+			System.out.println("Enter the year, comma, and beginning weekday-number to print your calendar (ex. '2017,1')");
+			Scanner sc = new Scanner(System.in);
+			try(){
+				
+			}
+			catch(){
+			}
+			}
+		}
+	}
+}
