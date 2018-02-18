2b693a69369bebb4400466e58016100ae242b1a2
aniskhan <anisa@khandkar.net>
2018-02-18 17:37:11 -0500

    working

--- HW2/3/src/Main.java
+++ HW2/3/src/Main.java
@@ -6,9 +6,19 @@ public class Main {
 		System.out.println("Enter a String:");
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
-		System.out.println(input);
-
+		int lenInput = input.length();
+		char[] charArray = input.toCharArray();
+		for (int i = 0; i < lenInput/2; i++) {
+			char head = charArray[i];
+			char tail = charArray[charArray.length - i -1];
+			if (head == tail) {
+				continue;
+			} else {
+				System.out.println("Not a palidrome.");
+				System.exit(0);
+			}
+		}
+		System.out.println("Yay a palidrome!");
 		sc.close();
 	}
-
 }
